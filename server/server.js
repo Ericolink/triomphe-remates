fc66c1bfbc5b067ada4f030f205db01d918fa6bf
ericolink <ericmunoz441@gmail.com>
2026-05-11 11:44:56 -0600

    feat: modelos de base de datos con relaciones (User, Property, Image, Lead, Analytics)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const sequelize = require('./config/db');
+const { sequelize } = require('./src/models/index');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
---
+++ server/src/models/Analytics.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../config/db');
+
+const Analytics = sequelize.define('Analytics', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  event: {
+    type: DataTypes.ENUM('view', 'contact', 'share', 'download'),
+    allowNull: false,
+  },
+  ip: {
+    type: DataTypes.STRING(45),
+    allowNull: true,
+  },
+  userAgent: {
+    type: DataTypes.STRING(500),
+    allowNull: true,
+  },
+  referrer: {
+    type: DataTypes.STRING(500),
+    allowNull: true,
+  },
+}, {
+  tableName: 'analytics',
+  timestamps: true,
+});
+
+module.exports = Analytics;
